df16cae8272bc7f09ad7c3b73038be42e0ec2b14
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-10-23 18:25:51 +0700

    up

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+//console.log('__dirname: ', __dirname)
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Template engine
+app.engine('hbs', engine({
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'));
+//console.log('PATH: ', path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
