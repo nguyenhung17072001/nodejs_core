d635950d4f492a27769c16eca3b907f3a92243cf
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-10-27 19:51:27 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ app.use(morgan('combined'))
 //console.log('__dirname: ', __dirname)
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 //Template engine
 app.engine('hbs', engine({
   extname: 'hbs'
@@ -20,10 +25,20 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log('req.body: ', req.body)
+  res.send('')
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
