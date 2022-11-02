252d1c68e3799a633fd3ccf818e377fc9c613924
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-11-02 22:31:36 +0700

    update

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator')
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 225 },
-    descripttion: { type: String, maxLength: 600 },
+    name: { type: String, required: true },
+    descripttion: { type: String },
     image: { type: String, maxLength: 225 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    slug: { type: String, slug: 'name' , unique: true},
+    //createdAt: { type: Date, default: Date.now },
+    //updatedAt: { type: Date, default: Date.now },
 
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
